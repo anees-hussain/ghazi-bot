af196a50df6a07f209e988ea5f2a4ff3cd29dacb
CH M Anees Hussain Toor <aneeshussain334@gmail.com>
2020-06-13 00:22:09 +0500

    "2nd Commit"

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bot = new TelegramBot(token, { polling: true });
 bot.onText(/\hi/, (msg, match) => {
   const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, "Hi, I am Ghazi.");
+  bot.sendMessage(chatId, "Hi, I am Ghazi Bot.");
 });
 
-//curl -F "url=https://ghazi.anees-hussain.now.sh"  https://api.telegram.org/bot1076997129:AAFt4ANG3nPQgj5uk8N-ceqyrSf5cMA_YkI/setWebhook
+//curl -F "url=https://ghazi-seven.now.sh/"  https://api.telegram.org/bot1076997129:AAFt4ANG3nPQgj5uk8N-ceqyrSf5cMA_YkI/setWebhook
