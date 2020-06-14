ae06703ae33c1ddacca83582297a4e81f44688a2
CH M Anees Hussain Toor <aneeshussain334@gmail.com>
2020-06-14 10:04:23 +0500

    Converted to previous version

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// require("dotenv").config();
-// const TelegramBot = require("node-telegram-bot-api");
-// const token = process.env.bot_token;
-// const bot = new TelegramBot(token, { polling: true });
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
+const token = process.env.bot_token;
+const bot = new TelegramBot(token, { polling: true });
 
-// bot.onText(/\hi/, (msg, match) => {
-//   const chatId = msg.chat.id;
+bot.onText(/\hi/, (msg, match) => {
+  const chatId = msg.chat.id;
 
-//   bot.sendMessage(chatId, "Hi, I am Ghazi Bot.");
-// });
-
-// module.exports = bot;
-
-const { Composer } = require("micro-bot");
-
-const bot = new Composer();
-
-bot.start((ctx) => {
-  ctx.reply("Bot has started");
+  bot.sendMessage(chatId, "Hi, I am Ghazi Bot.");
 });
+
+module.exports = bot;
+
+// const { Composer } = require("micro-bot");
+
+// const bot = new Composer();
+
+// bot.start((ctx) => {
+//   ctx.reply("Bot has started");
+// });
 
 //curl -F "url=https://ghazi-seven.now.sh/"  https://api.telegram.org/bot1076997129:AAFt4ANG3nPQgj5uk8N-ceqyrSf5cMA_YkI/setWebhook
 
